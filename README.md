528e40f6dd871c41f840b2a280508aa10e881a4f
Lewis Kim <lewis.k@berkeley.edu>
2018-09-01 12:06:46 -0700

    Added skeleton code to WoWGeneral, and updated README.md.

--- README.md
+++ README.md
@@ -5,18 +5,30 @@
 
 _*This project is in no way affiliated with Blizzard Entertainment._
 
-PH
+BlizzPy is a Blizzard API wrapper for Python. It fetches the raw json data from the Blizzard API in Python dictionaries, lists, or ``pandas`` dataframes, with many built-in functions that clean, process, and filter the data from the API for the user. This means that the users will always have the option to retrieve the raw API data as-is through BlizzPy, or can choose to retrieve them in more organized, specific data structures (e.g. choosing to retrieve a dictionary whose keys are guild member names and values are guild ranks, instead of processing the raw data themselves).
+
+BlizzPy currently supports the WoW Community API (for US, EU, and KR), with the D3 Community API, SC2 Community API, Game Data API, and WoW Game Data API being added very soon.
+
+For documentation, please visit [placeholder link]().
 
 ### Installation
 
 BlizzPy was written in Python 3.6, and will not work with Python 2.
 
+To install BlizzPy, simply run
+
+``placeholder code until blizzpy gets properly published.``
+
 Required packages:
 
-PH
+1) ``pandas`` (``pip3 install pandas``)
 
 ### References
 
 References to the libraries and packages used in BlizzPy:
 
-PH
+1) ``pandas`` (https://pandas.pydata.org/)
+2) ``urllib`` (https://docs.python.org/3/library/urllib.html)
+3) ``json`` (https://docs.python.org/3/library/json.html)
+4) ``requests`` (https://www.pythonforbeginners.com/requests/using-requests-in-python)
+
--- blizzpy/wowgeneral.py
+++ blizzpy/wowgeneral.py
@@ -36,12 +36,67 @@ class WoWGeneral:
 		self.api_key = api_key
 		self.locale = locale
 
+		self.achievements_data = {}
 
 
+	"""."""
+	def _get_achievement_data_url(self):
+		return
+
+
+	"""."""
+	def _get_boss_url(self):
+		return
+
+
+	"""."""
+	def _get_item_url(self):
+		return
+
+
+	"""."""
+	def _get_item_set_url(self):
+		return
+
+
+	"""."""
+	def _get_mount_url(self):
+		return
+
+
+	"""."""
+	def _get_quest_url(self):
+		return
+
+
+	"""."""
+	def _get_realm_url(self):
+		return
+
+
+	"""."""
+	def _get_recipe_url(self):
+		return
+
+
+	"""."""
+	def _get_spell_url(self):
+		return
+
+
+	"""."""
+	def _get_zone_url(self):
+		return
 
 
 
 ### Retrieving game achievements data. ###
+
+# https://us.api.battle.net/wow/achievement/2144?locale=en_US&apikey=6j49d54nvcttpbhvsqxs6pcvqf5b7t9y
+	
+	"""."""
+	def get_achievement_data(self, achievement_id):
+		return
 
 
 
@@ -49,7 +104,14 @@ class WoWGeneral:
 
 ### Retrieving game boss data. ###
 
+	"""."""
+	def get_bosses_data(self):
+		return
 
+
+	"""."""
+	def get_boss_info(self, boss_id):
+		return
 
 
 
@@ -57,6 +119,14 @@ class WoWGeneral:
 
 ### Retrieving game item data. ###
 
+	"""."""
+	def get_item_data(self, item_id):
+		return
+
+
+	"""."""
+	def get_item_set_data(self, itemSet_id):
+		return
 
 
 
@@ -65,27 +135,52 @@ class WoWGeneral:
 
 ### Retrieving game mount data. ###
 
+	"""."""
+	def get_mounts_data(self):
+		return
+
 
 
 
 ### Retrieving game quest data. ###
 
+	"""."""
+	def get_quest_data(self, quest_id):
+		return
 
+
+### Retrieving realm data. ###
+
+	"""."""
+	def get_realms_data(self):
+		return
 
 
 
 ### Retrieving game recipe data. ###
 
-
+	"""."""
+	def get_recipe_data(self):
+		return
 
 
 ### Retrieving game spell data. ###
 
-
-
+	"""."""
+	def get_spell_data(self):
+		return
 
 
 ### Retrieving game zone data. ###
+
+	"""."""
+	def get_zones_data(self):
+		return
+
+
+	"""."""
+	def get_zone_info(self, zone_id):
+		return
 
 
 
